6d2dc60caa83241b4ee6bfc5c5abb02165e7bbee
Matt Keuning <mattkeuning1@gmail.com>
2025-06-18 16:08:34 -0400

    adding exercise model

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Workout
+from .models import Exercise
 
 # Register your models here.
+
+admin.site.register(Workout)
+admin.site.register(Exercise)
--- workouts/models.py
+++ workouts/models.py
@@ -10,3 +10,12 @@ class Workout(models.Model):
 
     def __str__(self):
         return f'{self.date} - {self.exercise}'
+    
+class Exercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name='exercises')
+    name = models.CharField(max_length=100)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} ({self.sets}x{self.reps})'
